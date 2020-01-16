278ae10ff366a25d2c3ccbdb5334adcef84900fd
snehashis365 <snehashis.2000@gmail.com>
2020-01-16 23:45:12 +0530

    Driver Login and sign up pages intented for testing

--- app/src/main/java/com/hackaday/ambulance/MainActivity.java
+++ app/src/main/java/com/hackaday/ambulance/MainActivity.java
@@ -11,17 +11,28 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton bt_user;
+    ImageButton bt_driver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bt_user=(ImageButton)findViewById(R.id.bt_user);
+        bt_driver=(ImageButton)findViewById(R.id.bt_driver) ;
         bt_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, user_login.class);
-                startActivity(intent);
+                Intent user=new Intent(MainActivity.this, user_login.class);
+                startActivity(user);
+                finish();
+                return;
+            }
+        });
+        bt_driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent driver=new Intent(MainActivity.this, driver_login.class);
+                startActivity(driver);
                 finish();
                 return;
             }
--- app/src/main/java/com/hackaday/ambulance/driver_login.java
+++ app/src/main/java/com/hackaday/ambulance/driver_login.java
@@ -2,13 +2,29 @@ package com.hackaday.ambulance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class driver_login extends AppCompatActivity {
+
+    Button bt_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_login);
+
+        bt_signup=findViewById(R.id.bt_signup);
+        bt_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent driver_signup=new Intent(driver_login.this, com.hackaday.ambulance.driver_signup.class);
+                startActivity(driver_signup);
+                finish();
+                return;
+            }
+        });
     }
 }
