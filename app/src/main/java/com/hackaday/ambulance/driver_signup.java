ec59de9e878e6f3ad89975b6cccb2859b72120f5
snehashis365 <snehashis.2000@gmail.com>
2020-01-17 18:33:57 +0530

    Removed a spare ScrollView, ImageView popping up in driver signup page now fixed!!

--- app/src/main/java/com/hackaday/ambulance/driver_signup.java
+++ app/src/main/java/com/hackaday/ambulance/driver_signup.java
@@ -16,7 +16,7 @@ public class driver_signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_signup);
 
-        bt_submit=findViewById(R.id.bt_driver_submit);
+        bt_submit=findViewById(R.id.bt_user_submit);
         bt_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
