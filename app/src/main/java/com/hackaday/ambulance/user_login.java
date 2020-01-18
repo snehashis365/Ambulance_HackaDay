44b1daa5fd7228f4ff1784bcd80ee2c71d2d1457
snehashis365 <snehashis.2000@gmail.com>
2020-01-18 15:35:03 +0530

    Added Link to Databse for User Login and Signup Auto Login by default when signed up once or logged in once Please remeber your credentials once signed up Contact me in case you forget it

--- app/src/main/java/com/hackaday/ambulance/user_login.java
+++ app/src/main/java/com/hackaday/ambulance/user_login.java
@@ -1,24 +1,63 @@
 package com.hackaday.ambulance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class user_login extends AppCompatActivity {
 
     Button bt_signup,bt_login;
+    EditText userName, pass;
     TextView forgot;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    private void startMap(){
+        //Method to switch activity
+
+        Intent user_map=new Intent(user_login.this, User_Map.class);
+        startActivity(user_map);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        userName=findViewById(R.id.userName);
+        pass=findViewById(R.id.login_Password);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    Toast.makeText(user_login.this, "          Login Success with Firebase\nUser Login: Don't Judge Work in progress", Toast.LENGTH_SHORT).show();
+                    startMap();
+                    /*
+                    Intent user_map=new Intent(user_login.this, User_Map.class);
+                    startActivity(user_map);
+                    */
+                }
+
+            }
+        };
 
         bt_signup=findViewById(R.id.bt_signup);
         bt_login=findViewById(R.id.bt_login);
@@ -33,9 +72,30 @@ public class user_login extends AppCompatActivity {
         bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(user_login.this, "User Login: Don't Judge Work in progress", Toast.LENGTH_SHORT).show();
+                /*Toast.makeText(user_login.this, "User Login: Don't Judge Work in progress", Toast.LENGTH_SHORT).show();
                 Intent user_map=new Intent(user_login.this, User_Map.class);
-                startActivity(user_map);
+                startActivity(user_map);*/
+
+                final String UserName=userName.getText().toString().trim();
+                final String Password=pass.getText().toString();
+
+                if(UserName.length()>0 && Password.length()>0){
+                    mAuth.signInWithEmailAndPassword(UserName, Password).addOnCompleteListener(user_login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(user_login.this, "Login Failed! Please Try again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+                else{
+                    Toast.makeText(user_login.this, "Please enter credentials", Toast.LENGTH_SHORT).show();
+                    startMap();
+                    Toast.makeText(user_login.this, "Assuming tester Map opened for test", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
         forgot.setOnClickListener(new View.OnClickListener() {
@@ -44,5 +104,22 @@ public class user_login extends AppCompatActivity {
                 Toast.makeText(user_login.this, "User Reset Password: Coming Soon!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Starting Listener
+        mAuth.addAuthStateListener(firebaseAuthListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //Stopping Listener
+        mAuth.removeAuthStateListener(firebaseAuthListener);
     }
 }
--- app/src/main/java/com/hackaday/ambulance/user_signup.java
+++ app/src/main/java/com/hackaday/ambulance/user_signup.java
@@ -1,33 +1,131 @@
 package com.hackaday.ambulance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class user_signup extends AppCompatActivity {
 
-    EditText name, email, phone, Pass, rePass;
+    EditText name, email, phone, pass, rePass;
     Button user_signup;
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    private void startMap(){
+        //Method to switch activity
+
+        Intent user_map=new Intent(user_signup.this, User_Map.class);
+        startActivity(user_map);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_signup);
+        name=findViewById(R.id.signup_Name);
         email=findViewById(R.id.signup_email);
         phone=findViewById(R.id.signup_phone);
-        Pass=findViewById(R.id.signup_Password);
+        pass=findViewById(R.id.signup_Password);
         rePass=findViewById(R.id.signup_rePassword);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    Toast.makeText(user_signup.this, "Login after signup Success with Firebase\nUser Signup: Dont't Judge Work in progress", Toast.LENGTH_LONG).show();
+                    startMap();
+                }
+            }
+        };
+
         user_signup=findViewById(R.id.bt_user_submit);
         user_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(user_signup.this, "User Signup: Coming Soon!", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(user_signup.this, "User Signup: Coming Soon!", Toast.LENGTH_SHORT).show();
+
+                final String Name=name.getText().toString();
+                final String Email=email.getText().toString();
+                final String Phone=phone.getText().toString();
+                final String Password=pass.getText().toString();
+                final String RePassword=rePass.getText().toString();
+                if(Name.length()>=3){
+
+                    if(Email.length()>0 && Phone.length()>=10){
+                        if(Password.length()>0 && Password.equals(RePassword)){
+                            mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(user_signup.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if(task.isSuccessful()){
+                                        String user_id= mAuth.getCurrentUser().getUid();
+                                        DatabaseReference current_user_db= FirebaseDatabase.getInstance().getReference().child("Members").child("Users").child(user_id);
+                                        Map newUser = new HashMap();
+                                        newUser.put("Name", Name);
+                                        newUser.put("Phone", Phone);
+
+                                        current_user_db.setValue(newUser);
+
+                                    }
+                                    else{
+                                        Toast.makeText(user_signup.this, "User Signup: Failed!! PLease Try Again", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                        }
+                        else if (Password.length()<8){
+                            Toast.makeText(user_signup.this, "Password Must be atleast 8 characters long", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(user_signup.this, "Password don't match please Re-enter", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(user_signup.this, "Please enter valid Email and Phone nmuber", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else {
+                    Toast.makeText(user_signup.this, "Enter Valid Name 1st", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
     }
 }
