48148dcb6594ede4eab34b9f99d3281879633d72
snehashis365 <snehashis.2000@gmail.com>
2020-01-19 21:46:03 +0530

    Added Places autocomplete search bar will not work now for billing issues until that work for map activities will be at halt Database backend development for Drivers will begin from next commits as of today this is the nightly build will continue tomorrow

--- app/src/main/java/com/hackaday/ambulance/User_Map.java
+++ app/src/main/java/com/hackaday/ambulance/User_Map.java
@@ -1,6 +1,7 @@
 package com.hackaday.ambulance;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -14,11 +15,16 @@ import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -27,8 +33,26 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class User_Map extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,6 +62,11 @@ public class User_Map extends FragmentActivity implements OnMapReadyCallback {
     ImageButton btn_Logout;
     Location mLastLoacation;
     LocationRequest mLocationRequest;
+    View mapView;
+    //MaterialSearchBar materialSearchBar;
+    PlacesClient placesClient;
+    private List<AutocompletePrediction> predictionList;
+    private String destination;
 
 
     private void logOut(){
@@ -46,18 +75,8 @@ public class User_Map extends FragmentActivity implements OnMapReadyCallback {
         Toast.makeText(User_Map.this, "Session Ended", Toast.LENGTH_SHORT).show();
         //Intent logout=new Intent(User_Map.this, MainActivity.class);
         //startActivity(logout);
-        /*new CountDownTimer(3000,1){
-            @Override
-            public void onTick(long l) {
-
-            }
-
-            @Override
-            public void onFinish() {*/
-                finish();
-                return;
-            /*}
-        }.start();*/
+        finish();
+        return;
 
     }
 
@@ -75,26 +94,53 @@ public class User_Map extends FragmentActivity implements OnMapReadyCallback {
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
+        //Initializing API key to for Places SDK
+        Places.initialize(User_Map.this,"AIzaSyD_51pdZf8pAXCGT7Ti8fhWczuhbSFLFM0");
+
+        placesClient = Places.createClient(this);
+        //final AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
+
+        mapView = mapFragment.getView();
+
         btn_Logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                /*
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(User_Map.this, "Session Ended", Toast.LENGTH_SHORT).show();
-                //Intent logout=new Intent(User_Map.this, MainActivity.class);
-                //startActivity(logout);
-                finish();
-                return;
-                */
+                //Logout User
                 view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 logOut();
 
             }
         });
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+// Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+// Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                destination=place.getName().toString();
+
+                //Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                //Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+
     }
 
-    public void moveToUserLocation(){
+    private void moveToUserLocation(){
         mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
@@ -114,6 +160,18 @@ public class User_Map extends FragmentActivity implements OnMapReadyCallback {
 
     }
 
+    //Moves my location button by the provided margin arguments
+    private void moveMyLocationButton(int left,int top,int right, int bottom){
+
+        View locationButton = ((View)mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP,0);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        layoutParams.setMargins(left,top,right,bottom);
+
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -126,23 +184,11 @@ public class User_Map extends FragmentActivity implements OnMapReadyCallback {
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
 
                 moveToUserLocation();
-                /*mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if(location!=null){
-                            mLastLoacation = location;
-                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                            mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
-                        }
-                        else {
-                            Toast.makeText(User_Map.this, "Failed to get current location please try again", Toast.LENGTH_LONG).show();
-                            finish();
-                        }
-
-                    }
-                });*/
                 mMap.setMyLocationEnabled(true);
+
+                //Send my location button to bottom
+                moveMyLocationButton(0,0,18,100);
+
 
             }else{
                 checkLocationPermission();
