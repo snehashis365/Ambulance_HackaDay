3320923e8b1cfb4f42402d7975afb749899f45fb
snehashis365 <snehashis.2000@gmail.com>
2020-01-23 19:44:20 +0530

    Added Pin Location and Position fetcher The pin image view is fine tuned to pint at the center of the map all the time The Pin fetcher function will get the current camera position and store it continuously in a pin position object once the camera starts moving Once the camera stops moving the the pin position is saved to pin location object and hence the pin location is updated every time the map stops moving

--- app/src/main/java/com/hackaday/ambulance/User_Map.java
+++ app/src/main/java/com/hackaday/ambulance/User_Map.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -32,6 +33,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -56,195 +58,222 @@ import java.util.List;
 
 public class User_Map extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private  FusedLocationProviderClient mFusedLocationClient;
-    //GoogleApiClient mGoogleApiClient;
-    ImageButton btn_Logout;
-    Location mLastLoacation;
-    LocationRequest mLocationRequest;
-    View mapView;
-    //MaterialSearchBar materialSearchBar;
-    PlacesClient placesClient;
-    private List<AutocompletePrediction> predictionList;
-    private String destination;
+	private GoogleMap mMap;
+	private  FusedLocationProviderClient mFusedLocationClient;
+	ImageButton btn_Logout;
+	ImageView user_pin;
+	Location mLastLoacation;
+	Location pinLocation;
+	LocationRequest mLocationRequest;
+	LatLng pinPosition;
+	View mapView;
+	PlacesClient placesClient;
+	private List<AutocompletePrediction> predictionList;
+	private String destination;
 
 
-    private void logOut(){
+	private void logOut(){
 
-        FirebaseAuth.getInstance().signOut();
-        Toast.makeText(User_Map.this, "Session Ended", Toast.LENGTH_SHORT).show();
-        //Intent logout=new Intent(User_Map.this, MainActivity.class);
-        //startActivity(logout);
-        finish();
-        return;
+		FirebaseAuth.getInstance().signOut();
+		Toast.makeText(User_Map.this, "Session Ended", Toast.LENGTH_SHORT).show();
+		//Intent logout=new Intent(User_Map.this, MainActivity.class);
+		//startActivity(logout);
+		finish();
+		return;
 
-    }
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user__map);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user__map);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        btn_Logout=findViewById(R.id.btn_Logout);
+		mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+		btn_Logout=findViewById(R.id.btn_Logout);
+		user_pin=findViewById(R.id.user_pin);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.user_map);
-        assert mapFragment != null;
-        mapFragment.getMapAsync(this);
+		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.user_map);
+		assert mapFragment != null;
+		mapFragment.getMapAsync(this);
 
-        //Initializing API key to for Places SDK
-        Places.initialize(User_Map.this,"AIzaSyD_51pdZf8pAXCGT7Ti8fhWczuhbSFLFM0");
+		//Initializing API key to for Places SDK
+		Places.initialize(User_Map.this,"AIzaSyD_51pdZf8pAXCGT7Ti8fhWczuhbSFLFM0");
 
-        placesClient = Places.createClient(this);
-        //final AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
+		placesClient = Places.createClient(this);
+		//final AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
 
-        mapView = mapFragment.getView();
+		mapView = mapFragment.getView();
 
-        btn_Logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+		btn_Logout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
 
-                //Logout User
-                view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-                logOut();
+				//Logout User
+				view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+				logOut();
 
-            }
-        });
+			}
+		});
 
-        // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+		// Initialize the AutocompleteSupportFragment.
+		AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+				getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
 // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+		autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
 
 // Set up a PlaceSelectionListener to handle the response.
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                destination=place.getName().toString();
+		autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+			@Override
+			public void onPlaceSelected(Place place) {
+				// TODO: Get info about the selected place.
+				destination=place.getName().toString();
 
-                //Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
-            }
+				//Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+			}
 
-            @Override
-            public void onError(Status status) {
-                // TODO: Handle the error.
-                //Log.i(TAG, "An error occurred: " + status);
-            }
-        });
-
-
-    }
-
-    private void moveToUserLocation(){
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if(location!=null){
-                    mLastLoacation = location;
-                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
-                }
-                else {
-                    Toast.makeText(User_Map.this, "Failed to get current location please try again", Toast.LENGTH_LONG).show();
-                    finish();
-                }
-
-            }
-        });
-
-    }
-
-    //Moves my location button by the provided margin arguments
-    private void moveMyLocationButton(int left,int top,int right, int bottom){
-
-        View locationButton = ((View)mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP,0);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        layoutParams.setMargins(left,top,right,bottom);
-
-    }
+			@Override
+			public void onError(Status status) {
+				// TODO: Handle the error.
+				//Log.i(TAG, "An error occurred: " + status);
+			}
+		});
 
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(3000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	}
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+	private void moveToUserLocation(){
+		mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+			@Override
+			public void onSuccess(Location location) {
+				if(location!=null){
+					mLastLoacation = location;
+					pinLocation = location;
+					LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+					mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+					mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+				}
+				else {
+					Toast.makeText(User_Map.this, "Failed to get current location please try again", Toast.LENGTH_LONG).show();
+					finish();
+				}
 
-                moveToUserLocation();
-                mMap.setMyLocationEnabled(true);
+			}
+		});
 
-                //Send my location button to bottom
-                moveMyLocationButton(0,0,18,100);
+	}
+
+	//Moves my location button by the provided margin arguments
+	private void moveMyLocationButton(int left,int top,int right, int bottom){
+
+		View locationButton = ((View)mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+		layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP,0);
+		layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+		layoutParams.setMargins(left,top,right,bottom);
+
+	}
 
 
-            }else{
-                checkLocationPermission();
-            }
-        }
+	@Override
+	public void onMapReady(final GoogleMap googleMap) {
+		mMap = googleMap;
+		mLocationRequest = new LocationRequest();
+		mLocationRequest.setInterval(10000);
+		mLocationRequest.setFastestInterval(3000);
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+			if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+				moveToUserLocation();
+				mMap.setMyLocationEnabled(true);
+
+				//Send my location button to bottom
+				moveMyLocationButton(0,0,18,100);
 
 
-    }
+			}else{
+				checkLocationPermission();
+
+				moveToUserLocation();
+				mMap.setMyLocationEnabled(true);
+				//Send my location button to bottom
+				moveMyLocationButton(0,0,18,100);
+			}
+		}
+
+		mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+			@Override
+			public void onCameraMove() {
+				pinPosition=googleMap.getCameraPosition().target;
+			}
+		});
+		mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+			@Override
+			public void onCameraIdle() {
+				if (pinPosition!=null){
+					pinLocation.setLatitude(pinPosition.latitude);
+					pinLocation.setLongitude(pinPosition.longitude);
+					//Logs for testing
+					Log.d("Moved Pin to:","Latitude:"+pinLocation.getLatitude()+", Longitude:"+pinLocation.getLongitude());
+				}
+
+			}
+		});
 
 
-
-    private void checkLocationPermission() {
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
-                new AlertDialog.Builder(this).setTitle("Give Permission").setMessage("Give Permission Message").setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        ActivityCompat.requestPermissions(User_Map.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                    }
-                }).create().show();
-            }
-            else{
-                ActivityCompat.requestPermissions(User_Map.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case 1:{
-                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-                        moveToUserLocation();
-                        mMap.setMyLocationEnabled(true);
-                    }
-                }
-            }
-        }
-    }
+	}
 
 
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
+	private void checkLocationPermission() {
 
-    }
+		if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        logOut();
-    }
+			if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+				new AlertDialog.Builder(this).setTitle("Give Permission").setMessage("Give Permission Message").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+						ActivityCompat.requestPermissions(User_Map.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+					}
+				}).create().show();
+			}
+			else{
+				ActivityCompat.requestPermissions(User_Map.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+			}
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		switch (requestCode){
+			case 1:{
+				if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+					if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+						moveToUserLocation();
+						mMap.setMyLocationEnabled(true);
+					}
+				}
+			}
+		}
+	}
+
+
+
+	@Override
+	public void onPointerCaptureChanged(boolean hasCapture) {
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		logOut();
+	}
 }
 
